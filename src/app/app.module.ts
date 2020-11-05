42aac3bf3f0a1b3b7c1109ccf96320177129aebd
wana155 <apere129@ucr.edu>
2020-11-05 14:41:06 -0600

    Added home, assessment, and contact components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AssessmentComponent } from './assessment/assessment.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AssessmentComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
