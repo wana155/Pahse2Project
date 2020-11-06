4e35539da30acaf71e6448a2976c0ba69fd152b3
wana155 <apere129@ucr.edu>
2020-11-06 16:06:57 -0600

    added sub-components to get credentials and choose a quiz type

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -1,3 +1,4 @@
+import { flatten } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assessment.component.css']
 })
 export class AssessmentComponent implements OnInit {
+  first:string;
+  last:String;
+  candidateID:String;
 
-  constructor() { }
+  credentialsActive: boolean;
+  credentialsIno:boolean;
+  
+  questionsActive:boolean;
+  constructor() {
+    this.credentialsActive =true;
+    this.credentialsIno =true;
+    this.questionsActive = false;
+   }
 
   ngOnInit(): void {
   }
+  credentials(){
+    //Check here if the  redentials are valid
+    if (true)//if credentials are valid and match server records
+    {this.credentialsIno =false;
 
+    }
+    else{alert("Sorry, Credentials are invalid, Try again");
+        this.first='';
+        this.last='';
+        this.candidateID='';
+        }
+  }
+  option(i){
+    this.credentialsActive=false;
+    this.questionsActive = true;
+
+   //get the data for corret set of questions here
+
+  }
+ 
 }
