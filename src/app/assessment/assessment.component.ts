65a9c26c71bf40925d38a3d437d61769a310eb71
wana155 <apere129@ucr.edu>
2020-11-09 08:21:21 -0600

    Modified logic to display and record quiz questions and user answers. The assessment is shown one question at a time and the score is retrieved and shown to the user.

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -1,8 +1,10 @@
 import { flatten } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+
 import { from } from 'rxjs';
 import { CredentialServiceService } from '../credential-service.service';
 import { User } from '../credential-service.service';
+import { Quiz, QuizService } from '../quiz.service';
 
 @Component({
   selector: 'app-assessment',
@@ -12,46 +14,128 @@ import { User } from '../credential-service.service';
 export class AssessmentComponent implements OnInit {
   first:string;
   last:String;
-  candidateID:String;
+  candidateID:number;
+  correcta:number;
+  texti:String;
+
   allUsers:User[];
+  allQuestions: Quiz[];
+  currentIndex:number;
+  currentQuestion:Quiz;
+  reponses:number[];
 
   credentialsActive: boolean;
   credentialsIno:boolean;
-  
   questionsActive:boolean;
-  
-  constructor(private serv:CredentialServiceService) {
+  resultsActive:boolean;
+  submitb:boolean;
+  constructor(private serv:CredentialServiceService,private quizServ:QuizService) {
     this.credentialsActive =true;
     this.credentialsIno =true;
     this.questionsActive = false;
+    this.resultsActive =false;
+    this.submitb=true;
+
+    this.reponses = new Array();
+    this.currentIndex =0;
+    for (let i=0;i<10;i++){
+      this.reponses[i]=5;
+    }
    }
 
   ngOnInit(): void {
     this.serv.loadData().subscribe(data=>this.allUsers=data,err=>console.log("HERROR!!!"+err),()=>console.log("DONE!"));
+    this.quizServ.loadData().subscribe(data=>this.allQuestions=data,err=>console.log("HERROR_QUIZ"+err),()=>console.log("DONE!QUIZ"));
+    
     //console.log(this.allUsers.length);
-    //for(let user of this.allUsers){
-    //  console.log(user);}
    
   }
 
   credentials(){
     //Check here if the  redentials are valid
-    if (true)//if credentials are valid and match server records
+    let i=0; let foud=false;
+   for(i=0;i<this.allUsers.length;i++){
+        if (this.allUsers[i].fname==this.first.toLowerCase() && this.allUsers[i].lname==this.last.toLowerCase() && this.allUsers[i].candidateId==this.candidateID ){
+          foud = true;
+        }
+    }
+    if (foud)//if credentials are valid and match server records
     {this.credentialsIno =false;
-
     }
     else{alert("Sorry, Credentials are invalid, Try again");
         this.first='';
         this.last='';
-        this.candidateID='';
+        this.candidateID=null;
         }
   }
-  option(i){
+  
+
+  quizOption(i){
     this.credentialsActive=false;
     this.questionsActive = true;
-
    //get the data for corret set of questions here
+   //just set the index to option * 10 
+  }
+
+  anOption(opt){
+    this.reponses[this.currentIndex]=opt;
+
+    //Reset the active element
+    var contenedores=document.getElementById("conte");
+    var children = contenedores.children;
+ for (var i = 0; i < children.length; i++) {
+   var Child = children[i];
+     Child.setAttribute("class","formi");
+  }
+
+   if (opt==1){
+    var cate=document.getElementById("aone");
+    cate.setAttribute("class","list-group-item list-group-item-action active");}
+  if (opt==2){
+      var cate=document.getElementById("atwo");
+      cate.setAttribute("class","list-group-item list-group-item-action active");}
+  if (opt==3){
+        var cate=document.getElementById("athree");
+        cate.setAttribute("class","list-group-item list-group-item-action active");}
+  if (opt==4){
+    var cate=document.getElementById("afour");
+    cate.setAttribute("class","list-group-item list-group-item-action active");}
+
+ }
+
+  
+  next(){
+    if(this.currentIndex==9)
+    {
+      alert("You are about to submit this quiz!");
+        this.resultsActive=true;
+        this.questionsActive=false;
+        this.correcta=0;
+        for(let i=0;i<10;i++){
+          if (this.reponses[i]==this.allQuestions[i].correct)
+          {this.correcta++;}
+        }
+        if(this.correcta>=7)
+        {this.texti ="✅ Congratulations! You have passed the assessment."; }
+        else{this.texti =" ❌ UGH! You failed the test! You need at least 7 correct answers";}
+    }
+    else{
+
+    if(this.reponses[this.currentIndex]!=5) //deabilitate the button here
+    this.currentIndex++;
+      if (this.currentIndex==9){this.submitb=false;}
+
+     //Reset the active element
+    var contenedores=document.getElementById("conte");
+    var children = contenedores.children;
+ for (var i = 0; i < children.length; i++) {
+   var Child = children[i];
+     Child.setAttribute("class","formi");
+  }
+
+
+}
 
   }
- 
+
 }
